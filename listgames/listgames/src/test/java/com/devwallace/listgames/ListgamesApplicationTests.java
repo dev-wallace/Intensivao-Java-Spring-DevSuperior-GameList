fd5ce81d9e68b9c2ff8926966bb71cba8768bd11
yokaithefato <wallacewwc2015wwk@gmail.com>
2024-11-16 20:36:30 -0300

    first commit

---
+++ listgames/listgames/src/test/java/com/devwallace/listgames/ListgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devwallace.listgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
