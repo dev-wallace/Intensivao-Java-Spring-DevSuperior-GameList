2a4ff58f7741de19ea908128a503c21dbe20fcae
yokaithefato <wallacewwc2015wwk@gmail.com>
2024-11-17 14:31:16 -0300

    adicionando o findById

--- listgames/listgames/src/main/java/com/devwallace/listgames/services/GameService.java
+++ listgames/listgames/src/main/java/com/devwallace/listgames/services/GameService.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devwallace.listgames.dto.GameDTO;
 import com.devwallace.listgames.dto.GameMinDTO;
+import com.devwallace.listgames.entities.Game;
 import com.devwallace.listgames.repositories.GameRepository;
 
 @Service
@@ -16,7 +19,17 @@ public class GameService {
 private GameRepository gameRepository;
 
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
 
+        GameDTO dto = new GameDTO(result);
+
+        return dto;
+
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
 
         var result = gameRepository.findAll();
